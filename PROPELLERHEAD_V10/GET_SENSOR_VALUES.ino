d141ad248bd595332bc3ab4e4ce2fd934db14c19
realslimshady <chischte@gmx.ch>
2019-08-06 21:09:16 +0200

    initial commit

---
+++ PROPELLERHEAD_V10/GET_SENSOR_VALUES.ino
@@ -0,0 +1,107 @@
+int get_sensor_values()
+{
+  //*****************************************************************************
+  //I2C COMMUNICATION - GET RAW SENSOR VALUES FROM FXOS8700 (ACC)
+  //*****************************************************************************
+  int MPU_addr = 0x1F; // I2C address
+
+  Wire.setClock(400000); //to speed up I2c from 100 to 400kHz
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x03);  // starting with register 0x03
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 2, true); // request a total of 14 registers
+  ACC_GRAVITY_RAW = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+
+  //*****************************************************************************
+  //I2C COMMUNICATION - GET RAW SENSOR VALUES FROM FXAS21002C (GYRO)
+  //*****************************************************************************
+  MPU_addr = 0x21; // I2C address
+  Wire.setClock(400000); //to speed up I2c from 100 to 400kHz
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x01);  // starting with register 0x03//Library: Adafruit_FXAS21002C.h
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 2, true); // request a total of 14 registers
+  GYRO_RAW = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+
+  //*****************************************************************************
+  //CYCLESTOPWATCH RESULTS FOR THE MPU_6050 SECTION Wire.setClock(100000)
+  //1700 micros with and without all float stuff and sin/cos calculations
+  //1440 micros for two sensor values out of two I2C reads
+  //544micros if only one sensorvalue is communicated via I2C
+  //*****************************************************************************
+
+  //*****************************************************************************
+  //*****************************************************************************
+  //*****************************************************************************
+  //ACCELERATOR SENSOR SECTION
+  //USE THE ACC_GRAVITY SENSOR AS AN ANGLE SENSOR
+  //If the Motorarm is horizontal, the ACC_GRAVITY_RAW has 0G
+  //If the motorarm is vertical, it has 1G
+  //*****************************************************************************
+  //*****************************************************************************
+  //*****************************************************************************
+
+  float ACC_GRAVITY_MAX_G_VALUE = 8500; //MANUALLY WRITE THE SENSOR_VALUE WHEN SENSOR MEASURES FULL G-FORCE
+  //ACC_GRAVITY_RAW = ACC_GRAVITY_RAW * .1; //SCALE DOWN TO PREVENT INT OVERFLOATING
+
+  //LOW PASS FILTERING / SMOOTHING ACCELERATOR VALUESs
+  //*****************************************************************************
+  if (ACC_GRAVITY_RAW == ACC_GRAVITY_RAW) //FILTER OUT NaN's - ONLY NaN's DON'T PASS THIS TEST!
+  {
+    if (ACC_GRAVITY_RAW != 0) //ZEROS CAN BE CAUSED BY SENSOR ERROR
+    {
+      int ACC_SMOOTHCOUNTER = 3; //USE THIS VALUE TO ADAPT SMOOTHING
+      ACC_GRAVITY_LPF = (ACC_GRAVITY_LPF * (ACC_SMOOTHCOUNTER - 1) + ACC_GRAVITY_RAW) / ACC_SMOOTHCOUNTER;
+    }
+  }
+  //CONVERT ACCELERATOR VALUES TO ANGLE // unit [°/10] to avoid float
+  //*****************************************************************************
+  GRAVITY_ANGLE_LPF_NEW = asin(ACC_GRAVITY_LPF / (ACC_GRAVITY_MAX_G_VALUE)) * -65; //last factor and addend are experimentally determined
+
+  if (GRAVITY_ANGLE_LPF_NEW == GRAVITY_ANGLE_LPF_NEW)//FILTER OUT NaN's - ONLY NaN's DON'T PASS THIS TEST!
+  {
+    GRAVITY_ANGLE_LPF = GRAVITY_ANGLE_LPF_NEW;
+  }
+
+  //X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X
+  //X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X/X
+
+  //*****************************************************************************
+  //*****************************************************************************
+  //*****************************************************************************
+  //GYROSCOPE SENSOR SECTION
+  //USE THE GYRO SENSOR VALUES TO CALCULATE AN ANGLE
+  //THE OUTPUT OF THE GYRO SENSOR IS ANGULAR SPEED (NOT ACCELERATION)!
+  //*****************************************************************************
+  //*****************************************************************************
+  //*****************************************************************************
+
+  //CONVERT SENSOR UNIT TO ANGULAR SPEED UNIT [°/s]
+  //*****************************************************************************
+  //GYRO_RAW = GYRO_RAW + 30; //TO MINIMIZE MEASURED PERMANENT SENSOR ERROR
+  GYRO_ANGULAR_SPEED = GYRO_RAW * -0.0001 * 90;
+  //the last factor is to adjust to the desired the unit //check value via calculated angle
+///*
+   //SMOOTHING OF GYRO ANGULAR SPEED VALUES FOR THE PID REGULATOR (VIBRATIONS!)
+  //*****************************************************************************
+       int GYRO_SMOOTHCOUNTER = 1; //USE THIS VALUE TO ADAPT SMOOTHING
+      GYRO_ANGULAR_SPEED_SMOOTHED = (GYRO_ANGULAR_SPEED_SMOOTHED * (GYRO_SMOOTHCOUNTER - 1) + GYRO_ANGULAR_SPEED) / GYRO_SMOOTHCOUNTER;
+ //*/
+
+
+  //CALCULATING ANGLE: NEW_ANGLE = OLD_ANGLE + (ANGULAR_SPEED*DELTA_TIME)
+  //*****************************************************************************
+  transmission_delta_t = micros() - transmission_stopwatch;
+  GYRO_ANGLE = GYRO_ANGLE + (GYRO_ANGULAR_SPEED * (transmission_delta_t * 0.000001));
+  transmission_stopwatch = micros(); //RESTART THE CYCLE STOPWATCH
+
+
+  //THE GYRO ANGLE SHOULD SLOWLY DECAY TO THE VALUE OF THE ACCELERATOR ANGLE (AUTOCALIBRATION)
+  //*****************************************************************************
+  int GYRO_CALIBRATION_SMOOTHVALUE = 500; //HIGHER VALUE = SLOWER CALIBRATION
+
+  GYRO_ANGLE_CALIBRATED = (GYRO_ANGLE * (GYRO_CALIBRATION_SMOOTHVALUE - 1) + (GRAVITY_ANGLE_LPF)) / GYRO_CALIBRATION_SMOOTHVALUE;
+  GYRO_ANGLE = GYRO_ANGLE_CALIBRATED; //UPDATE THE ANGLEVALUE FOR THE NEXT LOOP
+
+  Serial.flush();
+}
---
+++ PROPELLERHEAD_V10/PROPELLERHEAD_V10.ino
@@ -0,0 +1,192 @@
+//*****************************************************************************
+//*****************************************************************************
+//*****************************************************************************
+//PROGRAMM TO CONTROL A PROPELLER TEST RIG WITH A PID REGULATOR
+//*****************************************************************************
+//*****************************************************************************
+//*****************************************************************************
+
+//*****************************************************************************
+//*****************************************************************************
+//PRE-SETUP SECTION / PIN LAYOUT
+//*****************************************************************************
+//*****************************************************************************
+#include<Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_FXAS21002C.h>
+#include <Adafruit_FXOS8700.h>
+#define ESC_pin 2
+//*****************************************************************************
+//MPU_6050 ACCELEROMETER / GYROSCOPE
+//*****************************************************************************
+//I2C PINS:
+//CONNECT SDA TO A4
+//CONNECT SCL TO A5
+//CONNECT XDA TO D2 //in this case not required
+//*****************************************************************************
+//KNOBS AND POTENTIOMETERS
+//*****************************************************************************
+#define toggle_knob 4
+#define manual_throttle_pot A0 // potentiometer control motorspeed in manual mode
+#define p_valuepot A6
+#define i_valuepot A1
+#define d_valuepot A2
+//*****************************************************************************
+//DECLARATION OF VARIABLES / DATA TYPES
+//*****************************************************************************
+//boolean (true/false)
+//byte (0-255)
+//int   (-32,768 to 32,767) / unsigned int: 0 to 65,535
+//long  (-2,147,483,648 to 2,147,483,647)
+//float (6-7 Digits)
+//*****************************************************************************
+boolean set_error_stopwatch_clearance = true;
+boolean autopilot = false;
+boolean pid_startupmode = true;
+
+byte one_at_a_time = 1;
+int setpoint = 0; //setpoint of the regulator is 0° //value slightli adapted because of sensor alignment
+
+int speedlimit_low = -200; // SET MAX DOWNWARDS SPEEDLIMIT [°/s]
+//int speedlimit_high = +250; //SET MAX UPWARDS SPEEDLIMIT [°/s]
+int startup_counter;//to startup the esc needs a longer delay between the pulses
+int kp_max = 1500; //[RPM /(°/10)] USE THIS VALUE TO SCALE THE REGULATOR POTENTIOMETER TO DESIRED RANGE
+int kp_factor;
+int ki_max = 8000; //[RPM /(°/10)/s] USE THIS VALUE TO SCALE THE REGULATOR POTENTIOMETER TO DESIRED RANGE
+int ki_factor;
+int kd_max = 3000; //[RPM /((°/10)/s))] UUSE THIS VALUE TO SCALE THE REGULATOR POTENTIOMETER TO DESIRED RANGE
+float kd_factor;
+int RPM_MAX = 10000; // just a rough guess
+int manual_throttle;
+float ACC_GRAVITY_LPF;
+float GRAVITY_ANGLE_LPF;
+float GYRO_ANGULAR_SPEED;
+float angle_error;
+
+int motor_pwm;
+int min_pwm_manual = 1200; // 1270 Motor brake // 1282 Motor start
+int min_pwm_autopilot = 1200;
+int max_pwm = 1540; //1450 sufficient // 1570 limit, faster sounds "unhealthy"
+
+//FOR ESC PROGRAMMING ONLY:
+//int min_pwm = 1000; // 1220 Motor off // 1245 Motor start
+//int max_pwm = ESC_pwm_period; //ATTENTION FOR ESC PROGRAMMING ONLY!!!!!
+
+long RPM_P;
+long RPM_I;
+float RPM_D;
+long RPM_SUM;
+
+unsigned long cyclestopwatch;
+unsigned long serialprinttimer;
+unsigned long previoustime;
+unsigned long stabilizationtime;
+unsigned long transmission_stopwatch;
+unsigned long motorpulse_stopwatch;
+unsigned long timemarktimer;
+unsigned long transmission_delta_t;
+unsigned long pid_delta_t;
+unsigned long newtime;
+float ACC_GRAVITY_RAW;
+float GRAVITY_ANGLE_LPF_NEW;
+float GYRO_RAW;
+float stopwatch = micros();
+float measured_angle;
+float GYRO_ANGLE;
+float GYRO_ANGLE_CALIBRATED;
+float cosinus_factor;
+float GYRO_ANGULAR_SPEED_SMOOTHED;
+
+//*****************************************************************************
+//******************######**#######*#######*#******#*######********************
+//*****************#********#**********#****#******#*#*****#*******************
+//******************####****#####******#****#******#*######********************
+//***********************#**#**********#****#******#*#*************************
+//*****************######***######*****#*****######**#*************************
+//*****************************************************************************
+void setup() {
+  //***************************************************************************
+  //SETUP GYROSCOPE / ACCELEROMETER / MPU_6050
+  //***************************************************************************
+
+  //CONNECT SCL TO A5
+  //CONNECT SDA TO A4
+  //CONNECT XDA TO D2
+  Adafruit_FXAS21002C gyro = Adafruit_FXAS21002C(0x0021002C);
+  Adafruit_FXOS8700 accelmag = Adafruit_FXOS8700(0x8700A, 0x8700B);
+
+  /* Initialise the sensor */
+  if (!gyro.begin())
+  {
+    /* There was a problem detecting the FXAS21002C ... check your connections */
+    while (1);
+  }
+
+  /* Initialise the sensor */
+  if (!accelmag.begin(ACCEL_RANGE_4G))
+  {
+    while (1);
+  }
+
+  //***************************************************************************
+  //SETUP PID REGULATOR
+  //***************************************************************************
+  pinMode(toggle_knob, INPUT_PULLUP);
+  pinMode(manual_throttle_pot, INPUT);
+  pinMode(p_valuepot, INPUT);
+  pinMode(i_valuepot, INPUT);
+  pinMode(d_valuepot, INPUT);
+  pinMode(ESC_pin, OUTPUT);
+  //***************************************************************************
+  Serial.begin(115200);//start serial connection
+  digitalWrite(ESC_pin, LOW);
+  Serial.println("EXIT SETUP");
+}
+//*****************************************************************************
+//*****************************************************************************
+//********************#*********#####***#####***######*************************
+//********************#********#*****#*#*****#**#*****#************************
+//********************#********#*****#*#*****#**######*************************
+//********************#********#*****#*#*****#**#******************************
+//********************#######***#####***#####***#******************************
+//*****************************************************************************
+//*****************************************************************************
+
+void loop() {
+
+  //***************************************************************************
+  //MAIN LOOP
+  //***************************************************************************
+
+  motorpulse_stopwatch = micros();
+  digitalWrite(ESC_pin, HIGH);
+  toggle_autopilot(); //SWITCH TO DESIRED OPERATION MODE
+  get_sensor_values(); //GET THE VALUES OF THE GYROSCOPE / ACCELEROMETER
+  pid_regulator(); //RUN THE PID REGULATOR LOOP
+  motorpulse_calculator(); //SEND DESIRED SPEED VALUES TO THE ESC (ELECTRONIC SPEED CONTROLLER)
+  while (micros() - motorpulse_stopwatch < motor_pwm)
+  {
+    //Wait until ESC pulse has the right length
+  }
+  digitalWrite(ESC_pin, LOW);
+  get_potentiometer_values();
+
+  //***************************************************************************
+  // serial_prints(); //ACTIVATE / DEACTIVATE SERIAL PRINTS FOR MONITORING AND DEBUGGING
+  //***************************************************************************
+  //Stopwatch two read the length of a programcycle
+  //***************************************************************************
+  /*
+    long runtime = micros() - stopwatch;
+    Serial.println(runtime);
+    //delay(300);
+    stopwatch = micros();
+  */
+  //***************************************************************************
+}
+//*****************************************************************************
+//*****************************************************************************
+//*****************************************************************************
+//*****************************************************************************
+//*****************************************************************************
+
